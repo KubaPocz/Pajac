24bc8a5497fb2753852daa1d2f554fb52c3d2331
Piotrrrrrrr2r <piotr.drygalski@edu.uekat.pl>
2025-11-12 15:38:18 +0100

    Added managment of stats
    Change of Stat text is not finished yet

--- Assets/Scripts/Management/StatsManager.cs
+++ Assets/Scripts/Management/StatsManager.cs
@@ -1,9 +1,15 @@
+using System;
 using UnityEngine;
 
 public class StatsManager : MonoBehaviour
 {
     public static StatsManager Instance;
     public CharacterInfo player;
+    private int StatPoints = 10;
+    private int[] StatisticsToPass = new int[5];
+    // Array containing statistics to pass towards CharacterInfo Methods
+    // Following this format: 0-Health 1-Stamina 2-Agility 3-Strenght 4-Precision
+    [SerializeField] public CharacterStats PlayerStats;
     private void Awake()
     {
         if (Instance != null)
@@ -18,4 +24,60 @@ public class StatsManager : MonoBehaviour
         player = GameManager.Instance.Player;
     }
 
+    public void AddHealthStatistic()
+    { 
+        if (StatPoints > 0) 
+        {
+            StatPoints--;
+            StatisticsToPass[0]++;
+        }
+    }
+    public void AddStaminaStatistic()
+    {
+        if (StatPoints > 0)
+        {
+            StatPoints--;
+            StatisticsToPass[1]++;
+        }
+    }
+    public void AddAgilityStatistic()
+    {
+        if (StatPoints > 0)
+        {
+            StatPoints--;
+            StatisticsToPass[2]++;
+        }
+    }
+    public void AddStrenghtStatistic()
+    {
+        if (StatPoints > 0)
+        {
+            StatPoints--;
+            StatisticsToPass[3]++;
+        }
+    }
+    public void AddPrecisionStatistic()
+    {
+
+        if (StatPoints > 0)
+        {
+            StatPoints--;
+            StatisticsToPass[4]++;
+        }
+    }
+
+    public void ResetPoints()
+    {
+        StatPoints = 10;
+        Array.Clear(StatisticsToPass, 0, StatisticsToPass.Length);
+    }
+
+    public void ConfirmStatistics() 
+    {
+        for (int i = StatisticsToPass[0]; i-- > 0;) PlayerStats.IncreaseHealth();
+        for (int i = StatisticsToPass[0]; i-- > 0;) PlayerStats.IncreaseStamina();
+        for (int i = StatisticsToPass[0]; i-- > 0;) PlayerStats.IncreaseAgility();
+        for (int i = StatisticsToPass[0]; i-- > 0;) PlayerStats.IncreaseStrenght();
+        for (int i = StatisticsToPass[0]; i-- > 0;) PlayerStats.IncreasePrecision();
+    }
 }
--- Assets/Scripts/UI/StatisticsMenuButtons.cs
+++ Assets/Scripts/UI/StatisticsMenuButtons.cs
@@ -1,10 +1,51 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class StatisticsMenuButtons : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+     
+    public Text HealthPointsText;
+    public Text StaminaPointsText;
+    public Text AgilityPointsText;
+    public Text StrenghtPointsText;
+    public Text PrecisionPointsText;
+    private int PointsLeft=10;
+
     public void StartFight()
     {
+        StatsManager.Instance.ConfirmStatistics();
         SceneManager.LoadScene("Fight");
+    }
+
+    private void OnHealthButtonClicked()
+    {
+        StatsManager.Instance.AddHealthStatistic();
+        
+
+    }
+
+    private void OnStaminaButtonClicked()
+    {
+        StatsManager.Instance.AddStaminaStatistic();
+    }
+
+    private void OnAgilityButtonClicked()
+    {
+        StatsManager.Instance.AddAgilityStatistic();
+    }
+
+    private void OnStrenghtButtonClicked()
+    {
+        StatsManager.Instance.AddStrenghtStatistic();
+    }
+
+    private void OnPrecisionButtonClicked()
+    {
+        StatsManager.Instance.AddPrecisionStatistic();
+    }
+
+    private void OnResetButtonClicked() 
+    {
+        StatsManager.Instance.ResetPoints();
     }
 }
