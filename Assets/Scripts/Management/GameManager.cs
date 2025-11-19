174cffb435f8fa9d8f17494245a27fd3e6abb9bd
Patryk Budniok <patrykbudniok@gmail.com>
2025-11-19 14:39:02 +0100

    fixes

--- Assets/Scripts/Management/GameManager.cs
+++ Assets/Scripts/Management/GameManager.cs
@@ -4,7 +4,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public CharacterStats Player;
-<<<<<<< Updated upstream
+
     public CharacterStats[] Enemies;
     public int CurrentEnemy = 0;
 
@@ -12,12 +12,6 @@ public class GameManager : MonoBehaviour
     //public CharacterStats Elephant;
     //public CharacterStats Clown;
     //public CharacterStats Boss;
-    
-=======
-
-    public CharacterStats[] Enemies;
-
->>>>>>> Stashed changes
     public void Awake()
     {
         if(Instance != null)
