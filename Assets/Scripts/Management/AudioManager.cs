c034c89f19c35d5e36b3d4279a4bebc4cc5e03aa
Piotrrrrrrr2r <piotr.drygalski@edu.uekat.pl>
2026-01-14 16:27:29 +0100

    audio partialy implemented

--- Assets/Scripts/Management/AudioManager.cs
+++ Assets/Scripts/Management/AudioManager.cs
@@ -2,12 +2,73 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    public AudioSource src;
-    public AudioClip SfxPop;
 
-    public void PlayPop()
+    public AudioClip soundEffect;
+    public AudioClip musicClip;
+    private AudioSource sfxSource;
+    private AudioSource musicSource;
+
+    private static AudioManager instance;
+
+    void Awake()
     {
-        src.clip = SfxPop;
-        src.Play();
+        // Singleton setup
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            Initialize();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        PlayMusic();
+    }
+
+    void Initialize()
+    {
+        sfxSource = gameObject.AddComponent<AudioSource>();
+        sfxSource.playOnAwake = false;
+
+        if (soundEffect != null)
+        {
+            sfxSource.clip = soundEffect;
+        }
+
+        musicSource = gameObject.AddComponent<AudioSource>();
+        musicSource.playOnAwake = false;
+
+        if (soundEffect != null)
+        {
+            musicSource.clip = musicClip;
+        }
+
+    }
+
+    // Static method to play sound from anywhere
+    public static void PlaySoundEffect()
+    {
+        if (instance != null && instance.sfxSource != null && instance.sfxSource.clip != null)
+        {
+            instance.sfxSource.Play();
+        }
+        else
+        {
+            Debug.LogWarning("PersistentAudioManager not initialized or no sound effect set!");
+        }
+    }
+
+    public static void PlayMusic()
+    {
+        if (instance != null && instance.musicSource != null && instance.musicSource.clip != null)
+        {
+            instance.musicSource.Play();
+        }
+        else
+        {
+            Debug.LogWarning("PersistentAudioManager not initialized or no sound effect set!");
+        }
     }
 }
